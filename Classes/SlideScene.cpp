c1797fdce22782f154a252ea3da552ca0e534870
Scarb <jjhfen00@163.com>
2016-04-06 20:22:56 +0800

    update something

--- Classes/SlideScene.cpp
+++ Classes/SlideScene.cpp
@@ -8,15 +8,15 @@ using namespace cocos2d::ui;
 using namespace cocostudio::timeline;
 using namespace cocos2d;
 
-Scene* SlideScene::createScene(int diff, int loop)
+Scene* SlideScene::createScene()
 {
 	auto scene = Scene::create();
-	auto layer = SlideScene::create(diff, loop);
+	auto layer = SlideScene::create();
 	scene->addChild(layer);
 	return scene;
 }
 
-bool SlideScene::init(int diff, int loop)
+bool SlideScene::init()
 {
 	if (!Layer::init())
 		return false;
@@ -31,22 +31,5 @@ bool SlideScene::init(int diff, int loop)
 		sp->setPosition(x, y);
 
 		this->addChild(sp);
-	}
-	
-
-}
-
-Layer* SlideScene::create(int diff, int loop)
-{
-	auto pRef = new SlideScene();
-	if (pRef && pRef->init(diff, loop))
-	{
-		pRef->autorelease();
-		return pRef;
-	}
-	else
-	{
-		CC_SAFE_DELETE(pRef);
-		return nullptr;
 	}
 }
